250cd1c9264935a20552e458c71dddd242eef822
GuilhermoReadonly <guilhermo.readonly@gmail.com>
2015-09-24 08:25:59 +0200

    Remove unused import

--- src/main/java/com/readonly/kimsufiavailability/KimsufiAvailableChecker.java
+++ src/main/java/com/readonly/kimsufiavailability/KimsufiAvailableChecker.java
@@ -2,14 +2,12 @@ package com.readonly.kimsufiavailability;
 
 import android.app.Activity;
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-import com.readonly.kimsufiavailability.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,17 +17,11 @@ import java.util.Date;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.URL;
 
 
 public class KimsufiAvailableChecker extends Activity {
